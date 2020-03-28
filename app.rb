f255d39b9561cdfefef11132599bdbbbfe6d987f
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-28 19:25:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,18 +1,43 @@
 require_relative 'config/environment'
 
+# class App < Sinatra::Base
+
+#   # This is a sample static route.
+#   get '/hello' do
+#     "Hello World!"
+#   end
+
+#   # This is a sample dynamic route.
+#   get "/hello/:name" do
+#     @user_name = params[:name]
+#     "Hello #{@user_name}!"
+#   end
+
+#   # Code your final two routes here:
+
+# end
+
 class App < Sinatra::Base
+	
+get '/goodbye/:name' do
+  @name = params[:name].to_s
+  
+  "Goodbye, #{@name}."
+end
 
-  # This is a sample static route.
-  get '/hello' do
-    "Hello World!"
+  get '/:operation/:number1/:number2' do
+    @oper = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    case @oper
+      when "subtract"
+        (@num1 - @num2).to_s
+      when "add"
+        (@num1 + @num2).to_s
+      when "multiply"
+        (@num1 * @num2).to_s
+      when "divide"
+        (@num1 / @num2).to_s
+    end
   end
-
-  # This is a sample dynamic route.
-  get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
-  end
-
-  # Code your final two routes here:
-
 end
